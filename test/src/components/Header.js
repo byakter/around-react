f89c2c5432ffe058b5d109593f5d1b04e4c3fd59
Barak Yakter <byakter@gmail,com>
2023-05-13 19:21:49 +0300

    Creating Header, Main and Footer components

---
+++ test/src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../images/Vector.svg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="around-the-us-logo" />
+    </header>
+  );
+};
+
+export default Header;
